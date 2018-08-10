ef5e062f6b6f189f2c107be2803c25545b193a0f
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-10 12:35:06 +0530

    Update 21.java

--- 21.java
+++ 21.java
@@ -1,14 +1,15 @@
-	class Ideone
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-  
-  int n,a,d,val=0;
+		int n,a,d,val=0;
 		Scanner sc=new Scanner(System.in);
 		n=sc.nextInt();
 		a=sc.nextInt();
 		d=sc.nextInt();
 	            val=(n*(2*a+(n-1)*d))/2;
                         System.out.println(val);
-                        }
-                        }
+
+// your code goes here
+	}
+}
